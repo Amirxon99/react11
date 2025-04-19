24d4225f60a420cb3dfb6031a3256d809fabdae9
Amirxon99 <114159472+Amirxon99@users.noreply.github.com>
2025-04-19 10:51:37 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Amirxon Ibaydillayev</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        placeat earum eius voluptas? Sed ducimus beatae animi dignissimos cum
+        facilis facere aspernatur id, unde ipsam et recusandae quibusdam nulla
+        quaerat!
+      </p>
+      <ul>
+         <li>HTML</li>
+         <li>CSS</li>
+         <li>Tailwind css</li>
+         <li>Javascript</li>
+         <li>React</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
